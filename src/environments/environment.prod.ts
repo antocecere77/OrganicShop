86429a6d23dce1c0c6f082321828992d759c1913
antocecere <antocecere77@gmail.com>
2019-12-14 18:18:14 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDWlmo23MdqAysKTHso0XHJLWuKrWhpmiw",
+    authDomain: "oshop-31631.firebaseapp.com",
+    databaseURL: "https://oshop-31631.firebaseio.com",
+    projectId: "oshop-31631",
+    storageBucket: "oshop-31631.appspot.com",
+    messagingSenderId: "137616350160",
+    appId: "1:137616350160:web:8010b6dd634662b2c537d5",
+    measurementId: "G-YLC07EG7JR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDWlmo23MdqAysKTHso0XHJLWuKrWhpmiw",
+    authDomain: "oshop-31631.firebaseapp.com",
+    databaseURL: "https://oshop-31631.firebaseio.com",
+    projectId: "oshop-31631",
+    storageBucket: "oshop-31631.appspot.com",
+    messagingSenderId: "137616350160",
+    appId: "1:137616350160:web:8010b6dd634662b2c537d5",
+    measurementId: "G-YLC07EG7JR"
+  }
 };

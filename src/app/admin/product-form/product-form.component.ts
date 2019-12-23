6b406580971fdfaacd262e96629810ed16df780f
antocecere <antocecere77@gmail.com>
2019-12-23 07:31:53 +0100

    Added product preview (bootstrap card)

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,7 +12,10 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) { 
+  constructor(
+      private router: Router, 
+      private categoryService: CategoryService, 
+      private productService: ProductService) { 
     this.categories$ = categoryService.getCategories();
   }
 
@@ -21,6 +25,7 @@ export class ProductFormComponent implements OnInit {
   save(product) {
     console.log(product);
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }

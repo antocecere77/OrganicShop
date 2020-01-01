82e85759f3f13cf2ea4db7f90c38e48245d11760
antocecere <antocecere77@gmail.com>
2020-01-01 18:19:43 +0100

    Fixing a design issue

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -2,7 +2,13 @@ import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {   
 
-  constructor(public items: ShoppingCartItem[]) {}
+  items: ShoppingCartItem[] = [];
+
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {  
+    for(let productId in itemsMap) {
+      this.items.push(itemsMap[productId])
+    }
+  }
 
   get productIds() {
     return Object.keys(this.items);

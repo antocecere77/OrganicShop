53f5ad0e7cd47b46129ecb9bc35de235564c9781
antocecere <antocecere77@gmail.com>
2020-01-05 15:40:09 +0100

    Importing and exporting module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from 'shared/shared.module';
+
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule,    
+    SharedModule,   
     NgbModule.forRoot(),
     RouterModule.forChild([])
   ],

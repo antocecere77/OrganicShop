713ac0146e3998606861fc561e67a31e619aee78
antocecere <antocecere77@gmail.com>
2020-01-05 15:16:43 +0100

    Creating the shopping cart module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,47 +8,30 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { CustomFormsModule } from 'ng2-validation';
-import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
-import { AuthGuard } from 'shared/services/auth-guard.service';
+import { SharedModule } from 'shared/shared.module';
 
 import { environment } from '../environments/environment';
-import { AdminOrdersComponent } from './admin/components/admin-orders/admin-orders.component';
-import { AdminProductsComponent } from './admin/components/admin-products/admin-products.component';
-import { ProductFormComponent } from './admin/components/product-form/product-form.component';
+import { AdminModule } from './admin/admin.module';
+import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { ProductsFilterComponent } from './products/products-filter/products-filter.component';
-import { ProductsComponent } from './products/products.component';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { SharedModule } from 'shared/shared.module';
-import { AdminModule } from './admin/admin.module';
+import { ProductsComponent } from './shopping/components/products/products.component';
+import { ShoppingModule } from './shopping/shopping.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
-    HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,   
-    LoginComponent,    
-    ProductsFilterComponent,    
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent
+    HomeComponent, 
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
+    ShoppingModule,
     FormsModule,
     CustomFormsModule,
     DataTableModule,
@@ -57,15 +40,8 @@ import { AdminModule } from './admin/admin.module';
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: ProductsComponent},
-      { path: 'products', component: ProductsComponent},
-      { path: 'shopping-cart', component: ShoppingCartComponent},
-      { path: 'login', component: LoginComponent},
-
-      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard]},
-      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard]},
-      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]}
-      
+      { path: '', component: ProductsComponent},     
+      { path: 'login', component: LoginComponent}      
     ])
   ],
   providers: [

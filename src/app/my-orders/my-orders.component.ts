bb26a258d5e0b8b20ba044becdb06988a6df2366
antocecere <antocecere77@gmail.com>
2020-01-05 09:40:13 +0100

    Displaying the order

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
 
-  constructor() { }
+  orders$;
+  
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) { 
 
-  ngOnInit() {
+    this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
   }
 
 }
